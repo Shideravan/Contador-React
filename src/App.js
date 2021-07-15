b8ece02afe6fa1512b36d8aaf9812a70a5608115
Shideravan <spannopan@yahoo.com.br>
2021-07-15 00:13:26 -0300

    Novas funções teste

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [valor, setValor] = useState(0);
+  const [texto, setTexto] = useState('O texto vai aparecer aqui');
+  const [enviado, setEnviado] = useState('N/A');
+  const [textArea, setTextArea] = useState('Clique aqui e coloque algo');
+
+  const handleContador = (valorer) => {
+    setValor(valorer + 1);
+  };
+
+  const handleTexto = (evento) => {
+    setTexto(evento.target.value);
+  };
+
+  const handleClick = (recebido) => {
+    setEnviado(recebido);
+  };
+
+  const handleTextBox = (evento) => {
+    setTextArea(evento.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Contador</h1>
+          <p>{valor}</p>
+          <button onClick={() => handleContador(valor)}>Clique</button>
+          <br></br>
+          <h1>Escrevedor</h1>
+          <p>{texto}</p>
+          <input
+            type="text"
+            name="barra"
+            placeholder="Digite um texto aqui"
+            onChange={handleTexto}
+          ></input>
+          <button onClick={() => handleClick(texto)}>
+            Clique para enviar o texto que está no Input Text
+          </button>
+          <p>Texto enviado: {enviado}</p>
+          <textarea onChange={handleTextBox} />
+          <br></br>
+          <button onClick={() => handleClick(textArea)}>
+            Clique para enviar o que está na Text Area
+          </button>
+        </div>
       </header>
     </div>
   );
